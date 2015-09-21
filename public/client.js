8ed52aafe5c32bf46fc890d7bcc851a37d7788d0
Drew Reynolds <NYDrewReynolds@gmail.com>
2015-09-21 11:40:22 -0600

    Add buttons & voting

--- public/client.js
+++ public/client.js
@@ -1,1 +1,20 @@
 var socket = io();
+
+var connectionCount = document.getElementById('connection-count');
+var statusMessage = document.getElementById('status-message');
+var buttons = document.querySelectorAll('#choices button');
+
+socket.on('usersConnected', function (count) {
+  connectionCount.innerText = 'Connected Users: ' + count;
+});
+
+socket.on('statusMessage', function (message) {
+  statusMessage.innerText = message;
+});
+
+for(var i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", function() {
+    console.log(this.innerText);
+    socket.send("voteCast", this.innerText);
+  });
+}
--- server.js
+++ server.js
@@ -17,4 +17,28 @@ server.listen(port, function () {
 });
 
 const io = socketIO(server);
+var votes = {};
+
+io.on('connection', function (socket) {
+  console.log('A user has connected. Current number of users:', io.engine.clientsCount);
+  io.sockets.emit('usersConnected', io.engine.clientsCount);
+
+  socket.emit("statusMessage", "You've connected wooo!")
+
+  socket.on('disconnect', function () {
+    console.log('A user has disconnected.', io.engine.clientsCount);
+    delete votes[socket.id];
+    console.log(votes);
+    io.sockets.emit('userConnection', io.engine.clientsCount);
+  });
+
+  socket.on('message', function (channel, message) {
+    if (channel === 'voteCast') {
+      votes[socket.id] = message;
+      console.log(votes);
+    }
+  });
+
+});
+
 module.exports = server;
